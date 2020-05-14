dadd3a8b400055d43c001e015aba9c54ab6ac192
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-05-15 00:46:07 +0800

    迴圈練習

---
+++ Unity_IfSwitchLoop/Assets/IfSwitchLoop.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class IfSwitchLoop : MonoBehaviour
+{
+    [Header("血量"), Range(0, 100)]
+    public int hp = 100;
+    [Header("道具")]
+    public string prop = "紅水";
+
+    public int i = 1;
+    private int j = 1;
+
+    [Header("地板")]
+    public GameObject cube;
+
+    private void CreateFloor(int length, int width)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int i = j; i < length; i++)
+            {
+
+                Instantiate(cube, new Vector3(j * 2, 0, i * 2), Quaternion.Euler(270, 0, 0));
+            }
+        }
+    }
+    private void Awake()
+    {
+        CreateFloor(10, 10);
+    }
+    private void Update()
+    {
+        if (hp >= 70)
+        {
+            print("安全");
+        }
+        else if (hp >= 20)
+        {
+            print("警告");
+        }
+        else
+        {
+            print("危險");
+        }
+    }        
+}
